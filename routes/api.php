3acd589f7caf75b670c475075904a1baccca3968
Jake Casto <jake@lynndigital.com>
2019-05-12 20:35:39 -0400

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -13,22 +13,20 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::group(['middleware' => 'auth.jwt', 'prefix' => 'api'], function() {
-	Route::group(['middleware' => 'api', 'prefix' => 'auth'], function() {
-	    Route::post('login', 'AuthController@login');
-	    Route::post('logout', 'AuthController@logout');
-	    Route::post('refresh', 'AuthController@refresh');
-	    Route::post('me', 'AuthController@me');
-	});
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function() {
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+});
 
-	Route::group(['prefix' => 'v1'], function() {
-		// model routes
-		Route::post('models/new', 'V1\ApiController@createModel');
-		Route::get('models/list', 'V1\ApiController@listModels');
-		Route::post('models/{identifier}/update', 'V1\ApiController@updateModel');
-		Route::post('models/{identifier}/delete', 'V1\ApiController@deleteModel');
-		Route::post('models/{identifier}/train', 'V1\ApiController@trainModel');
-		Route::post('models/{identifier}/predict', 'V1\ApiController@predictModel');
-		Route::get('models/{identifier}/predict', 'V1\ApiController@isTrainingModel');
-	});
+Route::group(['middleware' => 'auth.jwt', 'prefix' => 'v1'], function() {
+	// model routes
+	Route::post('models/new', 'V1\ApiController@createModel');
+	Route::get('models/list', 'V1\ApiController@listModels');
+	Route::post('models/{identifier}/update', 'V1\ApiController@updateModel');
+	Route::post('models/{identifier}/delete', 'V1\ApiController@deleteModel');
+	Route::post('models/{identifier}/train', 'V1\ApiController@trainModel');
+	Route::post('models/{identifier}/predict', 'V1\ApiController@predictModel');
+	Route::get('models/{identifier}/predict', 'V1\ApiController@isTrainingModel');
 });
