4467be2f62cb7f524bf634c1f21a97b50cd4f69b
Jake Casto <jake@lynndigital.com>
2019-05-16 14:50:26 -0400

    Work on dashboard

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard/{any}', function () {
-    return view('application');
-})->where('any', '.*');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
