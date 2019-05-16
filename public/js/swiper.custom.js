164aa1b033cfc441935e174fb5e1a74a461a6161
Jake Casto <jake@lynndigital.com>
2019-05-16 18:06:22 -0400

    Update swiper.custom.js

--- public/js/swiper.custom.js
+++ public/js/swiper.custom.js
@@ -4,15 +4,24 @@ Initialize Swiper
 "use strict";
 var swiper = new Swiper('.testimonials', {
   speed: 600,
-  slidesPerView: "auto",
-  spaceBetween: 0,
+  parallax: true,
   pagination: {
 	el: '.swiper-pagination',
 	dynamicBullets: true,
 	clickable : true
   },
-  navigation: {
-	nextEl: '.swiper-button-next',
-	prevEl: '.swiper-button-prev',
-  },
 });
+var swipert = new Swiper ('.hiw-titles', {
+spaceBetween: 1,
+centeredSlides: true,
+slidesPerView: 'auto',
+touchRatio: 1,
+slideToClickedSlide: true
+}); 
+var swiperc = new Swiper ('.hiw-content', {
+direction: 'horizontal',
+effect: 'slide'
+}); 
+
+swipert.controller.control = swiperc;
+swiperc.controller.control = swipert;
