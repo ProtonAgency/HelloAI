8e8e2702e8f8b16cd57449d7dafc1c9be17d2cf1
Jake Casto <jake@lynndigital.com>
2019-05-16 15:25:29 -0400

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,9 +36,4 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
-
-    protected function guard()
-    {
-        return auth()->guard('admins');
-    }
 }
